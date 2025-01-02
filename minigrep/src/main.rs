1873558ba73ca1142ab9b3bdcf40c38b376591d8
marcosdima <marcosdimatteo.dev@gmail.com>
2025-01-02 19:24:33 -0300

    first attempt

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::fs;
+
+#[derive(Debug)]
+struct Arguments {
+    target: String,
+    path: String,
+}
+
+fn get_args() -> Arguments {
+    // Get arguments from env.args...
+    let arguments: Vec<String> = env::args().collect();
+
+    // Returns the structure with the recollected data.
+    Arguments {
+        target: arguments[1].clone(),
+        path: arguments[2].clone()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = get_args();
+
+    println!("{arguments:#?}\n");
+
+    let text = fs::read_to_string(arguments.path)
+        .expect("File can't be readed...");
+
+    println!("The text is:\n{text}");
 }
